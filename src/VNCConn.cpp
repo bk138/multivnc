4e4bdf5be1feb9fbd5d3ef61af243f20461ae68f
Christian Beier <dontmind@freeshell.org>
2011-02-09 17:57:18 +0100

    Use higher limits for fallback mechanism.

--- src/VNCConn.cpp
+++ src/VNCConn.cpp
@@ -295,8 +295,8 @@ wxThread::ExitCode VNCConn::Entry()
 
 	  if(isMulticast())
 	    {
-	      // compute loss ratio: the way we do it here is per 1000 packets
-	      if(cl->multicastRcvd >= 1000 || (cl->multicastRcvd && cl->multicastTimeouts > 10))
+	      // compute loss ratio: the way we do it here is per 10000 packets
+	      if(cl->multicastRcvd >= 10000 || (cl->multicastRcvd && cl->multicastTimeouts > 10))
 		{
 		  multicastLossRatio = cl->multicastLost/(double)(cl->multicastRcvd + cl->multicastLost);
 		  cl->multicastRcvd = cl->multicastLost = 0;
@@ -314,7 +314,7 @@ wxThread::ExitCode VNCConn::Entry()
 		{
 		  rfbClientLog("MultiVNC: loss ratio > 0.2, requesting a full multicast framebuffer update\n");
 		  SendMulticastFramebufferUpdateRequest(cl, FALSE);
-		  cl->multicastLost -= cl->multicastLost/10;
+		  cl->multicastLost /= 2;
 		}
 	    }
 
