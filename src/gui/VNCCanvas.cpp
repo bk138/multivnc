bk138/multivnc

commit 7f7616f26a123eab6eb15902d7f4ae24553fdad2

Update TODO.

src/gui/VNCCanvas.cpp
@@ -351,7 +351,7 @@ void VNCCanvasContainer::setCanvas(VNCCanvas* c)
 {
   canvas = c;
 
-  wxBoxSizer* sizer_vert = new wxBoxSizer(wxHORIZONTAL);
+  wxBoxSizer* sizer_vert = new wxBoxSizer(wxVERTICAL);
   sizer_vert->Add(c, 3, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL);
   GetSizer()->Insert(0, sizer_vert, 1, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL);
 }

TODO
@@ -1,5 +1,9 @@
 
 in order of importance:
+ - maximization on win32
+ - stutter when using stats on win32
+ - win32 stats not the same as on UNIX?
+ - rethink the drawing model
  - fix hang when connection is disrupted
  - make VNC server on win32 exit when window is closed
  - fix TLS on win32, this is a problem in libvncclient

commit 39b18027edf91c316beb7d7c5c4feafb1c71dfd7

Fix bug accidently introduced with 7f7616f26a123eab6eb15902d7f4ae24553fdad2.

src/gui/VNCCanvas.cpp
@@ -352,7 +352,7 @@ void VNCCanvasContainer::setCanvas(VNCCanvas* c)
 {
   canvas = c;
 
-  wxBoxSizer* sizer_vert = new wxBoxSizer(wxVERTICAL);
+  wxBoxSizer* sizer_vert = new wxBoxSizer(wxHORIZONTAL);
   sizer_vert->Add(c, 3, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL);
   GetSizer()->Insert(0, sizer_vert, 1, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL);
 }