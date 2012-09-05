06cf2cc4590be38316a3a4f2f5c1becbc128900f
Christian Beier <dontmind@freeshell.org>
2012-09-06 00:58:47 +0200

    Android: load assets/help.html in Help Act.

--- android/src/com/coboltforge/dontmind/multivnc/HelpActivity.java
+++ android/src/com/coboltforge/dontmind/multivnc/HelpActivity.java
@@ -2,6 +2,7 @@ package com.coboltforge.dontmind.multivnc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class HelpActivity extends Activity {
 
@@ -9,7 +10,9 @@ public class HelpActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help);
-
+		WebView wv = (WebView) findViewById(R.id.helpwebView);
+		wv.loadUrl("file:///android_asset/help.html");
+		wv.getSettings().setBuiltInZoomControls(true);
 	
 	}
 	
