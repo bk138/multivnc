aa3ae3d6d7fa9dcdfa780b5ca867e26aacdb4aa8
Christian Beier <dontmind@freeshell.org>
2019-03-10 20:08:43 +0100

    Android: replace Canvas'es ActionBar with a Floating Action Button
    Closes #14

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/VncCanvasActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/VncCanvasActivity.java
@@ -27,9 +27,9 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import androidx.appcompat.app.AppCompatActivity;
 import android.text.ClipboardManager;
 import android.content.ContentValues;
 import android.content.DialogInterface;
@@ -50,15 +50,16 @@ import android.view.MotionEvent;
 import android.view.SoundEffectConstants;
 import android.view.VelocityTracker;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Toast;
 import android.view.inputmethod.InputMethodManager;
 import android.content.Context;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 @SuppressWarnings("deprecation")
-public class VncCanvasActivity extends AppCompatActivity {
+public class VncCanvasActivity extends Activity {
 
 
 	public class MightyInputHandler extends AbstractGestureInputHandler {
@@ -538,7 +539,7 @@ public class VncCanvasActivity extends AppCompatActivity {
 
 		super.onCreate(savedInstanceState);
 
-		// hide or show actionbar, title bar, status bar
+		// hide title bar, status bar
 		setupWindowSize();
 
 		setContentView(R.layout.canvas);
@@ -556,7 +557,17 @@ public class VncCanvasActivity extends AppCompatActivity {
 		inputHandler = new MightyInputHandler();
 		inputHandler.init();
 
-
+		/*
+		 * setup floating action button
+		 */
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			    Log.d(TAG, "FAB onClick");
+				openOptionsMenu();
+			}
+		});
 
 
 		/*
@@ -828,19 +839,6 @@ public class VncCanvasActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.vnccanvasactivitymenu, menu);
-
-		try {
-			if(getSupportActionBar().isShowing()) {
-				menu.findItem(R.id.itemSpecialKeys).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-				menu.findItem(R.id.itemSendKeyAgain).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-				menu.findItem(R.id.itemToggleKeyboard).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-				menu.findItem(R.id.itemSaveBookmark).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-				menu.findItem(R.id.itemDisconnect).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-			}
-		}
-		catch(NullPointerException e) {
-		}
-
 
 		return true;
 	}
@@ -1180,10 +1178,6 @@ public class VncCanvasActivity extends AppCompatActivity {
 		notificationToast.show();
 	}
 
-	public void setTitle(String text) {
-		getSupportActionBar().setTitle(text);
-	}
-
 
 	/**
 	 * Sets window size according to target device's platform.
@@ -1195,13 +1189,6 @@ public class VncCanvasActivity extends AppCompatActivity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		// disable home button as this sometimes takes keyboard focus
-		getSupportActionBar().setDisplayShowHomeEnabled(false);
-
-
-		// hideactionbar when there is a hardware menu button
-		if(ViewConfiguration.get(this).hasPermanentMenuKey())
-			getSupportActionBar().hide();
 	}
 
 	private void invalidateMyOptionsMenu() {
