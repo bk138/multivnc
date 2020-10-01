8032733c12ec0efebbb072ee3545c6e422f959ca
Christian Beier <cb@shoutrlabs.com>
2020-10-01 19:02:57 +0200

    Android: make prepareLibreSSL.sh work on MacOS
    re #92

--- prepareLibreSSL.sh
+++ prepareLibreSSL.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-MYDIR=$(dirname $(readlink -f "$0"))
+READLINK="readlink"
+SED="sed"
+if [ "$(uname)" = "Darwin" ]
+then
+    if [ -z "$(which greadlink)" ] || [ -z "$(which gsed)" ]
+    then
+        echo "On MacOS, make sure you have GNU coreutils installed. Bailing out."
+        exit 1
+    fi
+    READLINK="greadlink"
+    SED="gsed"
+fi
+
+MYDIR=$(dirname $($READLINK -f "$0"))
 
 cd "$MYDIR"/libressl
 echo libressl-v3.0.1 > OPENBSD_BRANCH
-sed -i 's/git pull --rebase//' update.sh
+$SED -i 's/git pull --rebase//' update.sh
 ./autogen.sh
