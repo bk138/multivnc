e831cc78d5449b0ea507409b874fda60ab522bab
Gaurav Ujjwal <gujjwal00@gmail.com>
2021-02-01 20:48:11 +0530

    Revert "Android: [im|ex]export DB native format instead of custom XML"
    This reverts commit 588664c6714ae7ad2544b20812ce07153b1431ce.

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ImportExportActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ImportExportActivity.java
@@ -20,11 +20,12 @@ import com.antlersoft.android.contentxml.SqliteElement;
 import com.antlersoft.android.contentxml.SqliteElement.ReplaceStrategy;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -54,19 +55,11 @@ public class ImportExportActivity extends Activity {
 			try {
 				URLConnection connection = new URL(urls[0]).openConnection();
 				connection.connect();
-				if(urls[0].endsWith(".xml")) {
-					Reader reader = new InputStreamReader(connection.getInputStream());
-					SqliteElement.importXmlStreamToDb(
-							mDatabase.getWritableDatabase(),
-							reader,
-							ReplaceStrategy.REPLACE_EXISTING);
-				} else {
-					mDatabase.close(); // Close any open database object
-					FileOutputStream dst = new FileOutputStream(mDatabase.getWritableDatabase().getPath());
-					Utils.copy(connection.getInputStream(), dst);
-					connection.getInputStream().close();
-					dst.close();
-				}
+				Reader reader = new InputStreamReader(connection.getInputStream());
+				SqliteElement.importXmlStreamToDb(
+						mDatabase.getWritableDatabase(),
+						reader,
+						ReplaceStrategy.REPLACE_EXISTING);
 				return null;
 			} catch (Exception e) {
 				return e;
@@ -112,7 +105,7 @@ public class ImportExportActivity extends Activity {
 			f = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 		}
 
-		f = new File(f, "MultiVNC-Export.sqlite");
+		f = new File(f, "MultiVNC-Export.xml");
 
 		_textSaveUrl.setText(f.getAbsolutePath());
 		try {
@@ -132,19 +125,17 @@ public class ImportExportActivity extends Activity {
 
 				try {
 					File f = new File(_textSaveUrl.getText().toString());
-
-					FileInputStream src = new FileInputStream(mDatabase.getReadableDatabase().getPath());
-					FileOutputStream dst = new FileOutputStream(f);
-					Utils.copy(src, dst);
-					src.close();
-					dst.close();
-
+					Writer writer = new OutputStreamWriter(new FileOutputStream(f, false));
+					SqliteElement.exportDbAsXmlToStream(mDatabase.getReadableDatabase(), writer);
+					writer.close();
 					finish();
 					Log.d(TAG, "export successful!");
 				}
 				catch (IOException ioe)
 				{
 					errorNotify("I/O Exception exporting config", ioe);
+				} catch (SAXException e) {
+					errorNotify("XML Exception exporting config", e);
 				}
 			}
 
