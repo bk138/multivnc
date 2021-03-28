ead8110270b262ff424a11e4c0c08034bad4ce7a
Gaurav Ujjwal <gujjwal00@gmail.com>
2021-03-28 10:46:40 +0530

    Android: Switch to JSON for Import/Export Importing from XML is still supported.

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/MetaListDao.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/MetaListDao.java
@@ -3,14 +3,21 @@ package com.coboltforge.dontmind.multivnc;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
 interface MetaListDao {
 
+    @Query("SELECT * FROM META_LIST WHERE _id = :id")
+    MetaList get(long id);
+
     @Insert
     long insert(MetaList list);
+
+    @Update
+    void update(MetaList list);
 
     @Query("SELECT * FROM META_LIST")
     List<MetaList> getAll();
