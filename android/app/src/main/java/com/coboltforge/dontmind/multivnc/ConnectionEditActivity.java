f5a930deb7f5a00f9856e5f989670cf6058753bf
Christian Beier <info@christianbeier.net>
2022-07-08 23:08:03 +0200

    Android: ConnectionEditActivity: cleanup

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ConnectionEditActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ConnectionEditActivity.java
@@ -1,14 +1,11 @@
 package com.coboltforge.dontmind.multivnc;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 /**
  * Bookmark editing activity.
@@ -16,8 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ConnectionEditActivity extends AppCompatActivity {
 	
 	private static final String TAG = "EditBookmarkActivity";
-	private VncDatabase database;
-	private ConnectionBean bookmark = new ConnectionBean();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,77 +20,42 @@ public class ConnectionEditActivity extends AppCompatActivity {
 
 		setContentView(R.layout.connection_edit_activity);
 
-		database = VncDatabase.getInstance(this);
+		VncDatabase database = VncDatabase.getInstance(this);
 
 		// default return value
 		setResult(RESULT_CANCELED);
 
-
 		// read connection from DB
-		Intent intent = getIntent();
-		long connID = intent.getLongExtra(Constants.CONNECTION, 0);
-		bookmark = database.getConnectionDao().get(connID);
-		if (bookmark != null)
-		{
+		long connID = getIntent().getLongExtra(Constants.CONNECTION, 0);
+		ConnectionBean bookmark = database.getConnectionDao().get(connID);
+		if (bookmark != null) {
 			Log.d(TAG, "Successfully read connection " + connID + " from database");
-
 			ConnectionEditFragment editor = (ConnectionEditFragment) getSupportFragmentManager().findFragmentById(R.id.connectionEditFragment);
 			if (editor != null) {
 				editor.setConnection(bookmark);
 			}
 		}
-		else 
-		{
+		else {
 			Log.e(TAG, "Error reading connection " + connID + " from database!");
 		}
 
 		Button saveButton = (Button) findViewById(R.id.buttonSaveBookmark);
-		saveButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-
+		saveButton.setOnClickListener(v -> {
+			try {
 				ConnectionEditFragment editor = (ConnectionEditFragment) getSupportFragmentManager().findFragmentById(R.id.connectionEditFragment);
-				bookmark = editor != null ? editor.getConnection() : null;
-				saveBookmark(bookmark);
-				
+				ConnectionBean conn = Objects.requireNonNull(editor).getConnection();
+				Log.d(TAG, "Saving bookmark for conn " + conn.id);
+				database.getConnectionDao().save(conn);
 				setResult(RESULT_OK);
-				finish();
 			}
+			catch(Exception e) {
+				Log.e(TAG, "Error saving bookmark: " + e.getMessage());
+			}
+			finish();
 		});
-		
 		
 		Button cancelButton = (Button) findViewById(R.id.buttonCancelBookmark);
-		cancelButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
-		
+		cancelButton.setOnClickListener(v -> finish());
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	        case android.R.id.home:
-	            // app icon in action bar clicked; go home
-	            Intent intent = new Intent(this, MainMenuActivity.class);
-	            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	            startActivity(intent);
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
-
-
-	private void saveBookmark(ConnectionBean conn) 	{
-		try {
-			Log.d(TAG, "Saving bookmark for conn " + conn.id);
-			database.getConnectionDao().save(conn);
-		}
-		catch(Exception e) {
-			Log.e(TAG, "Error saving bookmark: " + e.getMessage());
-		}
-	}
 }
