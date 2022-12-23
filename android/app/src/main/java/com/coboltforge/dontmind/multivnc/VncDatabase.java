11088fd6e26f7f1c3672b41254dbee894f12c2f2
Christian Beier <info@christianbeier.net>
2022-12-23 18:40:49 +0100

    Android: VncDatabase: change new columns to non-null in migra

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/VncDatabase.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/VncDatabase.java
@@ -136,9 +136,9 @@ public abstract class VncDatabase extends RoomDatabase {
             Log.i("VncDatabase", "Migrating to Room [13 -> 14]");
 
             // add new columns to CONNECTION_BEAN
-            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD ENCODINGSSTRING TEXT");
-            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD COMPRESSMODEL TEXT");
-            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD QUALITYMODEL TEXT");
+            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD ENCODINGSSTRING TEXT NOT NULL DEFAULT 'tight zrle ultra copyrect hextile zlib corre rre trle zywrle raw'");
+            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD COMPRESSMODEL TEXT NOT NULL DEFAULT 'L0'");
+            database.execSQL("ALTER TABLE CONNECTION_BEAN ADD QUALITYMODEL TEXT NOT NULL DEFAULT 'L5'");
             database.execSQL("ALTER TABLE CONNECTION_BEAN ADD SSH_HOST TEXT");
             database.execSQL("ALTER TABLE CONNECTION_BEAN ADD SSH_USERNAME TEXT");
             database.execSQL("ALTER TABLE CONNECTION_BEAN ADD SSH_PASSWORD TEXT");
