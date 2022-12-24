84103a83b3b71d050f4e00a480dfef0a1d9019be
Christian Beier <info@christianbeier.net>
2022-12-24 10:38:38 +0100

    Android: ConnectionEditFragment: clean up imports

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ConnectionEditFragment.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ConnectionEditFragment.java
@@ -1,6 +1,5 @@
 package com.coboltforge.dontmind.multivnc;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -18,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
