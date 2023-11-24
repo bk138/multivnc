64618d277131e4060ba55b01418d16fd8cc21464
Christian Beier <info@christianbeier.net>
2023-11-24 21:02:09 +0100

    Android: native: replace direct libssh2 use w/ libsshtunnel

--- android/app/src/main/cpp/vncconn.c
+++ android/app/src/main/cpp/vncconn.c
@@ -20,7 +20,7 @@
 #include <android/log.h>
 #include <netdb.h>
 #include <errno.h>
-#include <libssh2.h>
+#include <libsshtunnel.h>
 #include <rfb/rfbclient.h>
 
 #define TAG "VNCConn-native"
@@ -88,336 +88,6 @@ static void log_obj_tostring(JNIEnv *env, jobject obj, int prio, const char *for
     (*env)->ReleaseStringUTFChars(env, jStr, cStr);
 }
 
-/*
- * SSH-related stuff
- */
-
-static int onSshFingerprintCheck(const char *fingerprint, size_t fingerprint_len,
-                                 const char *host, rfbClient *client);
-
-
-typedef struct
-{
-    rfbClient *client;
-    LIBSSH2_SESSION *session;
-    pthread_t thread;
-    int ssh_sock;
-    int local_listensock;
-    int local_listenport;
-    char *remote_desthost;
-    int remote_destport;
-} SshData;
-
-
-THREAD_ROUTINE_RETURN_TYPE ssh_proxy_loop(void *arg)
-{
-    SshData *data = arg;
-    int rc, i;
-    struct sockaddr_in sin;
-    socklen_t sinlen;
-    LIBSSH2_CHANNEL *channel = NULL;
-    const char *shost;
-    int sport;
-    fd_set fds;
-    struct timeval tv;
-    ssize_t len, wr;
-    char buf[16384];
-    int proxy_sock = RFB_INVALID_SOCKET;
-
-    proxy_sock = accept(data->local_listensock, (struct sockaddr *)&sin, &sinlen);
-    if(proxy_sock == RFB_INVALID_SOCKET) {
-         __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: accept: %s\n", strerror(errno));
-        goto shutdown;
-    }
-
-    /* Close listener once a connection got accepted */
-    rfbCloseSocket(data->local_listensock);
-
-    shost = inet_ntoa(sin.sin_addr);
-    sport = ntohs(sin.sin_port);
-
-     __android_log_print(ANDROID_LOG_INFO, TAG,"ssh_proxy_loop: forwarding connection from %s:%d here to remote %s:%d\n",
-           shost, sport, data->remote_desthost, data->remote_destport);
-
-    channel = libssh2_channel_direct_tcpip_ex(data->session, data->remote_desthost,
-                                              data->remote_destport, shost, sport);
-    if(!channel) {
-         __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: Could not open the direct-tcpip channel!\n"
-                        "(Note that this can be a problem at the server!"
-                        " Please review the server logs.)\n");
-        goto shutdown;
-    }
-
-    /* Must use non-blocking IO hereafter due to the current libssh2 API */
-    libssh2_session_set_blocking(data->session, 0);
-
-    while(1) {
-        FD_ZERO(&fds);
-        FD_SET(proxy_sock, &fds);
-        tv.tv_sec = 0;
-        tv.tv_usec = 100000;
-        rc = select(proxy_sock + 1, &fds, NULL, NULL, &tv);
-        if(-1 == rc) {
-             __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: select: %s\n", strerror(errno));
-            goto shutdown;
-        }
-        if(rc && FD_ISSET(proxy_sock, &fds)) {
-            len = recv(proxy_sock, buf, sizeof(buf), 0);
-            if(len < 0) {
-                 __android_log_print(ANDROID_LOG_ERROR, TAG, "read: %s\n", strerror(errno));
-                goto shutdown;
-            }
-            else if(0 == len) {
-                 __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: the client at %s:%d disconnected!\n", shost,
-                        sport);
-                goto shutdown;
-            }
-            wr = 0;
-            while(wr < len) {
-                i = libssh2_channel_write(channel, buf + wr, len - wr);
-                if(LIBSSH2_ERROR_EAGAIN == i) {
-                    continue;
-                }
-                if(i < 0) {
-                     __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: libssh2_channel_write: %d\n", i);
-                    goto shutdown;
-                }
-                wr += i;
-            }
-        }
-        while(1) {
-            len = libssh2_channel_read(channel, buf, sizeof(buf));
-            if(LIBSSH2_ERROR_EAGAIN == len)
-                break;
-            else if(len < 0) {
-                 __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: libssh2_channel_read: %d\n", (int)len);
-                goto shutdown;
-            }
-            wr = 0;
-            while(wr < len) {
-                i = send(proxy_sock, buf + wr, len - wr, 0);
-                if(i <= 0) {
-                     __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: write: %s\n", strerror(errno));
-                    goto shutdown;
-                }
-                wr += i;
-            }
-            if(libssh2_channel_eof(channel)) {
-                 __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_proxy_loop: the server at %s:%d disconnected!\n",
-                        data->remote_desthost, data->remote_destport);
-                goto shutdown;
-            }
-        }
-    }
-
-    shutdown:
-
-     __android_log_print(ANDROID_LOG_INFO, TAG,"ssh_proxy_loop: shutting down\n");
-
-    rfbCloseSocket(proxy_sock);
-
-    if(channel)
-        libssh2_channel_free(channel);
-
-    libssh2_session_disconnect(data->session, "Client disconnecting normally");
-    libssh2_session_free(data->session);
-
-    rfbCloseSocket(data->ssh_sock);
-
-    return THREAD_ROUTINE_RETURN_VALUE;
-}
-
-
-/**
-   Creates an SSH tunnel and a local proxy and returns the port the proxy is listening on.
-   @return A pointer to an SshData structure or NULL on error.
- */
-SshData* ssh_tunnel_open(const char *ssh_host,
-                         const char *ssh_user,
-                         const char *ssh_password,
-                         const signed char *ssh_priv_key,
-                         int ssh_priv_key_len,
-                         const char *ssh_priv_key_password,
-                         const char *rfb_host,
-                         int rfb_port,
-                         rfbClient *client)
-{
-    int rc, i;
-    struct sockaddr_in sin;
-    socklen_t sinlen;
-    const char *fingerprint;
-    char *userauthlist;
-    struct addrinfo hints, *res;
-    SshData *data;
-
-    /* Sanity checks */
-    if(!ssh_host || !ssh_user || !rfb_host) /* these must be set */
-        return NULL;
-
-    data = calloc(1, sizeof(SshData));
-    // set the sockets to invalid so we don't close 0 inadvertently
-    data->local_listensock = data->ssh_sock = RFB_INVALID_SOCKET;
-
-    data->client = client;
-    data->remote_desthost = strdup(rfb_host); /* resolved by the server */
-    data->remote_destport = rfb_port;
-
-    /* Connect to SSH server */
-    data->ssh_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(data->ssh_sock == RFB_INVALID_SOCKET) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: socket: %s\n", strerror(errno));
-        goto error;
-    }
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family   = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rc = getaddrinfo(ssh_host, NULL, &hints, &res)) == 0) {
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = (((struct sockaddr_in *)res->ai_addr)->sin_addr.s_addr);
-        freeaddrinfo(res);
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: getaddrinfo: %s\n", gai_strerror(rc));
-        goto error;
-    }
-
-    sin.sin_port = htons(22);
-    if(connect(data->ssh_sock, (struct sockaddr*)(&sin), sizeof(struct sockaddr_in)) != 0) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: failed to connect to SSH server!\n");
-        goto error;
-    }
-
-    /* Create a session instance */
-    data->session = libssh2_session_init();
-    if(!data->session) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: could not initialize SSH session!\n");
-        goto error;
-    }
-
-    /* ... start it up. This will trade welcome banners, exchange keys,
-     * and setup crypto, compression, and MAC layers
-     */
-    rc = libssh2_session_handshake(data->session, data->ssh_sock);
-    if(rc) {
-        char *error_msg;
-        libssh2_session_last_error(data->session, &error_msg, NULL, 0);
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: error when starting up SSH session: %d: %s\n", rc, error_msg);
-        goto error;
-    }
-
-    /* At this point we havn't yet authenticated.  The first thing to do
-     * is check the hostkey's fingerprint against our known hosts Your app
-     * may have it hard coded, may go to a file, may present it to the
-     * user, that's your call
-     */
-    fingerprint = libssh2_hostkey_hash(data->session, LIBSSH2_HOSTKEY_HASH_SHA256);
-    if(onSshFingerprintCheck(fingerprint, 32, ssh_host, data->client) == -1) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: fingerprint check indicated tunnel setup stop\n");
-        goto error;
-    }
-
-    /* check what authentication methods are available */
-    userauthlist = libssh2_userauth_list(data->session, ssh_user, strlen(ssh_user));
-    __android_log_print(ANDROID_LOG_INFO, TAG,"ssh_tunnel_open: authentication methods: %s\n", userauthlist);
-
-    if(ssh_password && strstr(userauthlist, "password")) {
-        if(libssh2_userauth_password(data->session, ssh_user, ssh_password)) {
-            __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: authentication by password failed.\n");
-            goto error;
-        }
-    }
-    else if(ssh_priv_key && ssh_priv_key_password && strstr(userauthlist, "publickey")) {
-        if(libssh2_userauth_publickey_frommemory(data->session,
-                                                 ssh_user, strlen(ssh_user),
-                                                 NULL, 0,
-                                                 (const char*)ssh_priv_key, ssh_priv_key_len,
-                                                 ssh_priv_key_password)) {
-            __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: authentication by public key failed!\n");
-            goto error;
-        }
-    }
-    else {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: no supported authentication methods found!\n");
-        goto error;
-    }
-
-    /* Create and bind the local listening socket */
-    data->local_listensock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(data->local_listensock == RFB_INVALID_SOCKET) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: socket: %s\n", strerror(errno));
-        return NULL;
-    }
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(0); /* let the OS choose the port */
-    sin.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if(INADDR_NONE == sin.sin_addr.s_addr) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: inet_addr: %s\n", strerror(errno));
-        goto error;
-    }
-    sinlen = sizeof(sin);
-    if(-1 == bind(data->local_listensock, (struct sockaddr *)&sin, sinlen)) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "bind: %s\n", strerror(errno));
-        goto error;
-    }
-    if(-1 == listen(data->local_listensock, 1)) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "listen: %s\n", strerror(errno));
-        goto error;
-    }
-
-    /* get info back from OS */
-    if (getsockname(data->local_listensock, (struct sockaddr *)&sin, &sinlen ) == -1){
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: getsockname: %s\n", strerror(errno));
-        goto error;
-    }
-
-    data->local_listenport = ntohs(sin.sin_port);
-
-    __android_log_print(ANDROID_LOG_INFO, TAG,"ssh_tunnel_open: waiting for TCP connection on %s:%d...\n",
-           inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
-
-
-    /* Create the proxy thread */
-    if (pthread_create(&data->thread, NULL, ssh_proxy_loop, data) != 0) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "ssh_tunnel_open: proxy thread creation failed\n");
-        goto error;
-    }
-
-    return data;
-
-    error:
-    if (data->session) {
-        libssh2_session_disconnect(data->session, "Error in SSH tunnel setup");
-        libssh2_session_free(data->session);
-    }
-    if(data->remote_desthost)
-        free(data->remote_desthost);
-
-    rfbCloseSocket(data->local_listensock);
-    rfbCloseSocket(data->ssh_sock);
-
-    free(data);
-
-    return NULL;
-}
-
-
-void ssh_tunnel_close(SshData *data) {
-    if(!data)
-        return;
-
-    /* the proxy thread does the internal cleanup as it can be
-       ended due to external reasons */
-    THREAD_JOIN(data->thread);
-
-    if(data->remote_desthost)
-        free(data->remote_desthost);
-
-    free(data);
-
-     __android_log_print(ANDROID_LOG_INFO, TAG,"ssh_tunnel_close: done\n");
-}
-
 
 /*
  * The VM calls JNI_OnLoad when the native library is loaded (for example, through System.loadLibrary).
@@ -431,10 +101,9 @@ JNIEXPORT jint JNI_OnLoad(JavaVM __unused * vm, void __unused * reserved) {
     rfbClientLog = logcat_info;
     rfbClientErr = logcat_err;
 
-    // not thread safe according to https://www.libssh2.org/libssh2_init.html
-    // so run here once
-    if(libssh2_init(0)) {
-        __android_log_print(ANDROID_LOG_ERROR, TAG, "libssh2 initialization failed\n");
+    // this is not thread-safe so run here once
+    if(ssh_tunnel_init()) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "libsshtunnel initialization failed\n");
     }
 
     return JNI_VERSION_1_6;
@@ -644,6 +313,10 @@ static rfbBool onNewFBSize(rfbClient *client)
     return defaultMallocFramebuffer(client);
 }
 
+static void onSshError(void *client, ssh_tunnel_error_t error_code,  const char *error_message) {
+    __android_log_print(ANDROID_LOG_ERROR, TAG,"libsshtunnel error: %d - %s", error_code, error_message);
+}
+
 /**
    Decide whether or not the SSH tunnel setup should continue
    based on the current host and its fingerprint.
@@ -652,8 +325,10 @@ static rfbBool onNewFBSize(rfbClient *client)
    @return -1 if tunnel setup should be aborted
             0 if tunnel setup should continue
  */
-static int onSshFingerprintCheck(const char *fingerprint, size_t fingerprint_len,
-                                 const char *host, rfbClient *client)
+static int onSshFingerprintCheck(void *client,
+                                 const char *fingerprint,
+                                 int fingerprint_len,
+                                 const char *host)
 {
     if(!client) {
         __android_log_print(ANDROID_LOG_ERROR, TAG, "onSshFingerprintCheck failed due to client NULL");
@@ -808,18 +483,18 @@ JNIEXPORT jboolean JNICALL Java_com_coboltforge_dontmind_multivnc_VNCConn_rfbIni
     if(is_ssh_connection) {
         log_obj_tostring(env, obj, ANDROID_LOG_INFO, "rfbInit() setting up SSH-tunneled connection");
         // ssh-tunneling, check whether it's password- or key-based
-        SshData *data;
+        ssh_tunnel_t *data;
         if(cSshPassword) {
             // password-based
-            data = ssh_tunnel_open(cSshHost, cSshUser, cSshPassword, NULL, 0, NULL, cHost, port, cl);
+            data = ssh_tunnel_open_with_password(cSshHost, cSshUser, cSshPassword, cHost, port, cl, onSshFingerprintCheck, onSshError);
         } else {
             // key-based
-            data = ssh_tunnel_open(cSshHost, cSshUser, NULL, cSshPrivKey, cSshPrivKeyLen, cSshPrivKeyPassword, cHost, port, cl);
+            data = ssh_tunnel_open_with_privkey(cSshHost, cSshUser, (char*)cSshPrivKey, cSshPrivKeyLen, cSshPrivKeyPassword, cHost, port, cl, onSshFingerprintCheck, onSshError);
         }
 
         cl->serverHost = strdup("127.0.0.1");
         if(data) // might be NULL if ssh setup failed
-            cl->serverPort = data->local_listenport;
+            cl->serverPort = ssh_tunnel_get_port(data);
         rfbClientSetClientData(cl, VNCCONN_SSH_ID, data);
     } else {
         log_obj_tostring(env, obj, ANDROID_LOG_INFO, "rfbInit() setting up direct connection");
