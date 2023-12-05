b35f9dc5927c37843b10b0612426163d2b669f91
Christian Beier <info@christianbeier.net>
2023-12-05 10:41:39 +0100

    build: update LibreSSL submodule

--- prepareLibreSSL.sh
+++ prepareLibreSSL.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
 READLINK="readlink"
-SED="sed"
 if [ "$(uname)" = "Darwin" ]
 then
-    if [ -z "$(which greadlink)" ] || [ -z "$(which gsed)" ]
+    if [ -z "$(which greadlink)" ]
     then
         echo "On MacOS, make sure you have GNU coreutils installed. Bailing out."
         exit 1
     fi
     READLINK="greadlink"
-    SED="gsed"
 fi
 
 MYDIR=$(dirname $($READLINK -f "$0"))
 
 cd "$MYDIR"/libressl
-echo libressl-v3.0.1 > OPENBSD_BRANCH
-$SED -i 's/git pull --rebase//' update.sh
 ./autogen.sh
