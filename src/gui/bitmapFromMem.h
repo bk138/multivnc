3c912ac136d3ff5a48678a19fd7b649363850f7e
Christian Beier <info@christianbeier.net>
2024-10-06 18:02:00 +0200

    gui: remove bitmapFromMem macro
    Not needed anymore and this is in wxWidgets upstream meanwhile.
    
    re #222

--- src/gui/bitmapFromMem.h
+++ src/gui/bitmapFromMem.h
@@ -1,18 +1,9 @@
 #ifndef BITMAPFROMMEM_H
 #define BITMAPFROMMEM_H
 
-#include <wx/mstream.h>
 #include <wx/bmpbndl.h>
 #include <wx/filename.h>
 #include <wx/stdpaths.h>
-
-#define bitmapFromMem(name) _GetBitmapFromMemory(name, sizeof(name))
-
-inline wxBitmap _GetBitmapFromMemory(const unsigned char *data, int length) 
-{
-   wxMemoryInputStream is(data, length);
-   return wxBitmap(wxImage(is, wxBITMAP_TYPE_ANY, -1), -1);
-}
 
 #define bitmapBundleFromSVGResource(name) wxBitmapBundle::FromSVGFile(wxFileName(wxStandardPaths::Get().GetResourcesDir(), name, "svg").GetFullPath(), wxSize(24,24))
 
