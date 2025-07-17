fee502eb26add31e84d8772b374d267349c00a98
Christian Beier <info@christianbeier.net>
2025-07-17 18:28:01 +0200

    Android: ConnectionEditActivity: adapt for forced EdgeToEdge
    ...on Android 15+
    
    https://developer.android.com/about/versions/15/behavior-changes-15#window-insets
    has background info on this.
    
    We set android:fitsSystemWindows=true to not draw under the system bars
    and call EdgeToEdge.enable() for correct status bar text colour (i.e. not
    white on white) on Android 15 and newer.

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/ConnectionEditActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/ConnectionEditActivity.java
@@ -1,8 +1,11 @@
 package com.coboltforge.dontmind.multivnc.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.coboltforge.dontmind.multivnc.Constants;
@@ -22,6 +25,12 @@ public class ConnectionEditActivity extends AppCompatActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// On Android 15 and later, calling enableEdgeToEdge ensures system bar icon colors update
+		// when the device theme changes. Because calling it on pre-Android 15 has the side effect of
+		// enabling EdgeToEdge there as well, we only use it on Android 15 and later.
+		if (Build.VERSION.SDK_INT >= 35) {
+			EdgeToEdge.enable(this);
+		}
 
 		setContentView(R.layout.connection_edit_activity);
 
