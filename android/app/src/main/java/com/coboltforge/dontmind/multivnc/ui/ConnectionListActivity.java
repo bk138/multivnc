ba0f7ebf1c6235771f362bf5a37afd6584cbfb9e
Christian Beier <info@christianbeier.net>
2025-07-17 22:30:31 +0200

    Android: update ConnectionListActivity to ListFragment

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/ConnectionListActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/ConnectionListActivity.java
@@ -18,7 +18,6 @@
  */
 package com.coboltforge.dontmind.multivnc.ui;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.content.Intent.ShortcutIconResource;
 import android.database.Cursor;
@@ -27,6 +26,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.ListFragment;
 
 import com.coboltforge.dontmind.multivnc.Constants;
 import com.coboltforge.dontmind.multivnc.R;
@@ -37,69 +41,79 @@ import com.coboltforge.dontmind.multivnc.db.VncDatabase;
  * @author Michael A. MacDonald
  *
  */
-public class ConnectionListActivity extends ListActivity {
-	
-	VncDatabase database;
+public class ConnectionListActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.connection_list_activity);
 
-        database = VncDatabase.getInstance(this);
-        Cursor cursor = database.getConnectionDao().getAllAsCursor();
-        startManagingCursor(cursor);
-
-        // Now create a new list adapter bound to the cursor. 
-        // SimpleListAdapter is designed for binding to a Cursor.
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-                this, // Context.
-                R.layout.connection_list_item,
-                cursor,                                    // Pass in the cursor to bind to.
-                new String[] {
-                		ConnectionBean.GEN_FIELD_NICKNAME,
-                		ConnectionBean.GEN_FIELD_ADDRESS,
-                		ConnectionBean.GEN_FIELD_PORT,
-                		ConnectionBean.GEN_FIELD_REPEATERID }, // Array of cursor columns to bind to.
-                new int[] {
-                	R.id.list_text_nickname,
-                	R.id.list_text_address,
-                	R.id.list_text_port,
-                	R.id.list_text_repeater
-                });                                 // Parallel array of which template objects to bind to those columns.
-
-        // Bind to our new adapter.
-        setListAdapter(adapter);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, new ConnectionListFragment())
+                    .commit();
+        }
     }
 
-	/* (non-Javadoc)
-	 * @see android.app.ListActivity#onListItemClick(android.widget.ListView, android.view.View, int, long)
-	 */
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-        ConnectionBean connection = database.getConnectionDao().get(id);
-        if (connection != null) {
-            // create shortcut if requested
-            ShortcutIconResource icon = Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher);
+    public static class ConnectionListFragment extends ListFragment {
 
-            Intent intent = new Intent();
-            
-            Intent launchIntent = new Intent(this,VncCanvasActivity.class);
-            Uri.Builder builder = new Uri.Builder();
-            builder.authority(Constants.CONNECTION + ":" + connection.id);
-            builder.scheme("vnc");
-            launchIntent.setData(builder.build());
-            
-            intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-            intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, connection.nickname);
-            intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+        private VncDatabase database;
 
-            setResult(RESULT_OK, intent);
-		}
-		else
-			setResult(RESULT_CANCELED);
-		
-		finish();
-	}
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            setEmptyText(getString(R.string.shortcut_no_entries));
+
+            database = VncDatabase.getInstance(getActivity());
+            Cursor cursor = database.getConnectionDao().getAllAsCursor();
+            getActivity().startManagingCursor(cursor);
+
+            // Now create a new list adapter bound to the cursor.
+            // SimpleListAdapter is designed for binding to a Cursor.
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                    getActivity(), // Context
+                    R.layout.connection_list_item,
+                    cursor,        // Pass in the cursor to bind to.
+                    new String[] {
+                            ConnectionBean.GEN_FIELD_NICKNAME,
+                            ConnectionBean.GEN_FIELD_ADDRESS,
+                            ConnectionBean.GEN_FIELD_PORT,
+                            ConnectionBean.GEN_FIELD_REPEATERID }, // Array of cursor columns to bind to.
+                    new int[] {
+                            R.id.list_text_nickname,
+                            R.id.list_text_address,
+                            R.id.list_text_port,
+                            R.id.list_text_repeater
+                    });  // Parallel array of which template objects to bind to those columns.
+            // Bind to our new adapter.
+            setListAdapter(adapter);
+        }
+
+        @Override
+        public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+            ConnectionBean connection = database.getConnectionDao().get(id);
+            if (connection != null) {
+                // Create shortcut if requested
+                ShortcutIconResource icon = Intent.ShortcutIconResource.fromContext(getActivity(), R.drawable.ic_launcher);
+                Intent intent = new Intent();
+
+                Intent launchIntent = new Intent(getActivity(), VncCanvasActivity.class);
+                Uri.Builder builder = new Uri.Builder();
+                builder.authority(Constants.CONNECTION + ":" + connection.id);
+                builder.scheme("vnc");
+                launchIntent.setData(builder.build());
+
+                intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
+                intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, connection.nickname);
+                intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+                getActivity().setResult(AppCompatActivity.RESULT_OK, intent);
+            } else {
+                getActivity().setResult(AppCompatActivity.RESULT_CANCELED);
+            }
+
+            getActivity().finish();
+        }
+    }
 }
