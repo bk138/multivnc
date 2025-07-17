47987e4e4636f46e21fff084a6672ae46a2957ea
Christian Beier <info@christianbeier.net>
2025-07-17 18:23:35 +0200

    Android: HelpActivity: adapt for forced EdgeToEdge on Android 15+
    https://developer.android.com/about/versions/15/behavior-changes-15#window-insets
    has background info on this.
    
    We set android:fitsSystemWindows=true to not draw under the system bars
    and call EdgeToEdge.enable() for correct status bar text colour (i.e. not
    white on white) on Android 15 and newer.

--- android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/HelpActivity.java
+++ android/app/src/main/java/com/coboltforge/dontmind/multivnc/ui/HelpActivity.java
@@ -1,16 +1,25 @@
 package com.coboltforge.dontmind.multivnc.ui;
 
-import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.coboltforge.dontmind.multivnc.R;
 
-public class HelpActivity extends Activity {
+public class HelpActivity extends AppCompatActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// On Android 15 and later, calling enableEdgeToEdge ensures system bar icon colors update
+		// when the device theme changes. Because calling it on pre-Android 15 has the side effect of
+		// enabling EdgeToEdge there as well, we only use it on Android 15 and later.
+		if (Build.VERSION.SDK_INT >= 35) {
+			EdgeToEdge.enable(this);
+		}
 		setContentView(R.layout.help_activity);
 		WebView wv = findViewById(R.id.helpwebView);
 		wv.loadUrl("file:///android_asset/help.html");
