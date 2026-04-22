b6f298bcf728e115952b34a04af39968c141427d
Christian Beier <info@christianbeier.net>
2026-04-22 07:27:49 +0200

    KeyboardGrabber: handle return values in GTK version

--- src/gui/keyboardgrab/KeyboardGrabberGtk.cpp
+++ src/gui/keyboardgrab/KeyboardGrabberGtk.cpp
@@ -23,10 +23,13 @@ void KeyboardGrabber::grab(wxWindow* window)
   if(m_grab)
     return;
 
-  gdk_keyboard_grab(gtk_widget_get_window(window->GetHandle()), True, GDK_CURRENT_TIME);
-
-  m_grab = (void*)0x1; // Grab active
-  wxLogDebug("%s", __func__);
+  GdkGrabStatus status = gdk_keyboard_grab(gtk_widget_get_window(window->GetHandle()), True, GDK_CURRENT_TIME);
+  if(status == GDK_GRAB_SUCCESS) {
+      m_grab = (void*)0x1; // Grab active
+      wxLogDebug("KeyboardGrabber::grab: successful");
+  } else {
+      wxLogDebug("KeyboardGrabber::grab: failed with status %d", status);
+  }
 }
 
 
@@ -35,5 +38,5 @@ void KeyboardGrabber::ungrab()
   gdk_keyboard_ungrab(GDK_CURRENT_TIME);
 
   m_grab = nullptr;
-  wxLogDebug("%s", __func__);
+  wxLogDebug("KeyboardGrabber::ungrab");
 }
